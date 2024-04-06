5de9c099fb0f804f88a28e19238a6c947e321eb2
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-04-06 14:10:15 +0700

    push back-end project

---
+++ Back_End_BookApp/src/main/java/org/example/back_end_bookapp/BackEndBookAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.back_end_bookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndBookAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackEndBookAppApplication.class, args);
+  }
+
+}
---
+++ Back_End_BookApp/src/test/java/org/example/back_end_bookapp/BackEndBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.back_end_bookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndBookAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
