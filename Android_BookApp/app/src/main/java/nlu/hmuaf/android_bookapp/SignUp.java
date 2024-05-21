740beb56365d3bb3376d38005152361ae2a81cbe
HoangMinhTuan <21130600@st.hcmuaf.edu.vn>
2024-05-21 15:05:04 +0700

    up code

--- Android_BookApp/app/src/main/java/nlu/hmuaf/android_bookapp/SignUp.java
+++ Android_BookApp/app/src/main/java/nlu/hmuaf/android_bookapp/SignUp.java
@@ -11,53 +11,41 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUp extends AppCompatActivity {
-    private EditText signupEmail, signupPassword;
+    private EditText signupEmail, signupPassword, signupUserName;
     private Button signupButton;
-    private TextView loginRedirectText;
+    private TextView signUpRedirectText;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        signupUserName = findViewById(R.id.signup_username);
         signupEmail = findViewById(R.id.signup_email);
         signupPassword = findViewById(R.id.signup_password);
         signupButton = findViewById(R.id.signup_button);
-        loginRedirectText = findViewById(R.id.loginRedirectText);
 
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String user = signupEmail.getText().toString().trim();
+                String userName = signupUserName.getText().toString().trim();
+                String email = signupEmail.getText().toString().trim();
                 String pass = signupPassword.getText().toString().trim();
-                if (user.isEmpty()){
-                    signupEmail.setError("Email cannot be empty");
-                }
-                if (pass.isEmpty()){
-                    signupPassword.setError("Password cannot be empty");
-                } else{
-//                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//                        @Override
-//                        public void onComplete(@NonNull Task<AuthResult> task) {
-//                            if (task.isSuccessful()) {
-//                                Toast.makeText(SignUp.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
-//                                startActivity(new Intent(SignUp.this, Login.class));
-//                            } else {
-//                                Toast.makeText(SignUp.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-//                            }
-//                        }
-//                    }
-                    String email1 = signupEmail.getText().toString();
-                    Toast.makeText(SignUp.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(SignUp.this, Login.class));
-
+                if (userName.isEmpty() && email.isEmpty() && pass.isEmpty()) {
+                    Toast.makeText(SignUp.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(SignUp.this, Login.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(SignUp.this, "Invalid", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+
+        signUpRedirectText = findViewById(R.id.signup_RedirectText);
+        signUpRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(SignUp.this, Login.class));
-            }
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);            }
         });
     }
 }
