f520ec883d5eeaa4abeea07c6deae9a3291758b1
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-06-02 09:29:57 +0700

    Push payment method

--- BackEnd/src/main/java/nlu/hcmuaf/android_bookapp/enums/EPaymentMethod.java
+++ BackEnd/src/main/java/nlu/hcmuaf/android_bookapp/enums/EPaymentMethod.java
@@ -1,7 +1,7 @@
 package nlu.hcmuaf.android_bookapp.enums;
 
 public enum EPaymentMethod {
-  CREDIT_CART("Credit Card"), PAYPAL("PayPal"), VNPAY("VNPAY"), MOMO("MoMo");
+  CREDIT_CART("Credit Card"), COD("COD"), BANK_CART("Bank Card"), MOMO("MoMo");
   private final String text;
 
   private EPaymentMethod(String text) {
