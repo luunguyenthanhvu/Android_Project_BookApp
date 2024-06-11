b3b80eb6a045f3a5072503b13b056d997f6c78e5
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-06-12 00:31:18 +0700

    update code

--- BackEnd/src/main/java/nlu/hcmuaf/android_bookapp/service/templates/IBookService.java
+++ BackEnd/src/main/java/nlu/hcmuaf/android_bookapp/service/templates/IBookService.java
@@ -1,6 +1,12 @@
 package nlu.hcmuaf.android_bookapp.service.templates;
 
+import nlu.hcmuaf.android_bookapp.dto.response.ListBookResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface IBookService {
 
   void loadDefaultData();
+
+  Page<ListBookResponseDTO> getNewBookList(Pageable pageable);
 }
