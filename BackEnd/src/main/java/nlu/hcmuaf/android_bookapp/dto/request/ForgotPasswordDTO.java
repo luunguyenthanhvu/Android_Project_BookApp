09333e74cf7bd08103e6e018d07f6fcc1987572a
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-06-12 17:49:54 +0700

    push api forgot pass

---
+++ BackEnd/src/main/java/nlu/hcmuaf/android_bookapp/dto/request/ForgotPasswordDTO.java
@@ -0,0 +1,15 @@
+package nlu.hcmuaf.android_bookapp.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ForgotPasswordDTO {
+
+  private String email;
+}
