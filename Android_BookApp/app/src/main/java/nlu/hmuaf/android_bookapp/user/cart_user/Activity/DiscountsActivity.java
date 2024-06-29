4c764fb673d4d9569d9f019d37a075f99310e23d
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-06-29 16:42:11 +0700

    push source

--- Android_BookApp/app/src/main/java/nlu/hmuaf/android_bookapp/user/cart_user/Activity/DiscountsActivity.java
+++ Android_BookApp/app/src/main/java/nlu/hmuaf/android_bookapp/user/cart_user/Activity/DiscountsActivity.java
@@ -9,7 +9,8 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.shuhart.stepview.StepView;
+import com.anton46.stepsview.StepsView;
+//import com.shuhart.stepview.StepView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +19,13 @@ import nlu.hmuaf.android_bookapp.user.cart_user.fragment_front_end.FragmentListD
 import nlu.hmuaf.android_bookapp.R;
 
 public class DiscountsActivity extends AppCompatActivity {
-    private StepView stepView;
+    private StepsView stepView;
     private List<String> listStepView = new ArrayList<>();
 
     private Toolbar toolbar;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.discounts_activity);
 
@@ -36,9 +38,16 @@ public class DiscountsActivity extends AppCompatActivity {
         listStepView.add("Payment");
         listStepView.add("Summary");
 
-        stepView = findViewById(R.id.stepViewInDiscountActivity);
-        stepView.getState().animationType(StepView.ANIMATION_ALL).steps(listStepView).animationDuration(getResources().getInteger(android.R.integer.config_shortAnimTime)).commit();
-        stepView.go(0, true);
+//        stepView = findViewById(R.id.stepViewInDiscountActivity);
+//        stepView.getState().animationType(StepView.ANIMATION_ALL).steps(listStepView).animationDuration(getResources().getInteger(android.R.integer.config_shortAnimTime)).commit();
+//        stepView.go(0, true);
+        stepView.setLabels((String[]) listStepView.toArray()) // Đặt các bước (labels) cho StepsView từ listStepView
+                .setBarColorIndicator(getApplicationContext().getColor(android.R.color.darker_gray)) // Đặt màu của thanh chỉ báo
+                .setProgressColorIndicator(getApplicationContext().getColor(android.R.color.black)) // Đặt màu của chỉ báo tiến độ
+                .setLabelColorIndicator(getApplicationContext().getColor(android.R.color.black)) // Đặt màu của chỉ báo nhãn
+                .setCompletedPosition(3) // Đặt vị trí đã hoàn thành (nếu cần, ví dụ: 3)
+                .drawView(); // Vẽ StepsView
+
 
         // Tạo instance của Fragment
         FragmentListDiscountUser fragmentDiscountsUser = new FragmentListDiscountUser();
@@ -51,6 +60,7 @@ public class DiscountsActivity extends AppCompatActivity {
         fragmentTransaction.add(R.id.fragment_container, fragmentDiscountsUser);
         fragmentTransaction.commit();
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
