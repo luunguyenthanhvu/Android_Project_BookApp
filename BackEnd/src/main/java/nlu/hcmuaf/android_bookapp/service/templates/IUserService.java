1a1e561640db5a559653d915d43508d34c51597e
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-06-30 17:14:26 +0700

    update data

--- BackEnd/src/main/java/nlu/hcmuaf/android_bookapp/service/templates/IUserService.java
+++ BackEnd/src/main/java/nlu/hcmuaf/android_bookapp/service/templates/IUserService.java
@@ -1,9 +1,11 @@
 package nlu.hcmuaf.android_bookapp.service.templates;
 
+import java.util.List;
 import nlu.hcmuaf.android_bookapp.dto.request.ForgotPasswordDTO;
 import nlu.hcmuaf.android_bookapp.dto.request.LoginRequestDTO;
 import nlu.hcmuaf.android_bookapp.dto.request.RegisterRequestDTO;
 import nlu.hcmuaf.android_bookapp.dto.request.VerifyRequestDTO;
+import nlu.hcmuaf.android_bookapp.dto.response.ListAddressResponseDTO;
 import nlu.hcmuaf.android_bookapp.dto.response.MessageResponseDTO;
 import nlu.hcmuaf.android_bookapp.dto.response.TokenResponseDTO;
 
@@ -17,4 +19,5 @@ public interface IUserService extends IDataInitializer {
 
   MessageResponseDTO forgotPassword(ForgotPasswordDTO requestDTO);
 
+  List<ListAddressResponseDTO> getListAddress(long userId);
 }
